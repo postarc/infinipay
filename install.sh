f1f78c47105eceef395cb77789ae93de1fc9cd9f
postarc <45208608+postarc@users.noreply.github.com>
2018-11-23 20:35:16 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -142,15 +142,9 @@ if [[ $EUID -eq 0 ]]; then
    exit 1
 fi
 
-if [ -n "$(pidof $COIN_DAEMON)" ] $$ [ -d "/home/$USER/$CONFIGFOLDER" ] ; then
+if [ -n "$(pidof $COIN_DAEMON)" ] && [ -d "/home/$USER/$CONFIGFOLDER" ] ; then
   echo -e "${GREEN}$COIN_NAME is already installed.${NC}"
   exit 1
-fi
-if [ -n "$(pidof $COIN_DAEMON)" ] ; then
-  echo -e "$COIN_DAEMON daemon runing"
-fi
-if [ -d "/home/$USER/$CONFIGFOLDER" ]; then
-    echo -e "home/$USER/$CONFIGFOLDER exist"
 fi
 }
 
