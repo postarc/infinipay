7935f055041ddf6a9a2d7177c030d9726930f01e
postarc <45208608+postarc@users.noreply.github.com>
2018-12-21 19:21:45 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -179,7 +179,7 @@ fi
 function ifp_autorun() {
 #setup cron
 crontab -l > tempcron
-echo "@reboot $COIN_DAEMON -daemon" >> tempcron
+echo "@reboot /usr/local/bin/$COIN_DAEMON -daemon" >> tempcron
 crontab tempcron
 rm tempcron
 }
